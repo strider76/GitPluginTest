66a8ea53f0009ed9befb10c8c7596aee15629d7c
Manuel Millán <manuel@betangible.com>
2017-05-12 11:50:39 +0200

    nuevo commit en la rama nueva bug#1234

--- src/prueba/ClasePrueba.java
+++ src/prueba/ClasePrueba.java
@@ -5,6 +5,7 @@ public class ClasePrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello!!");
+		System.out.println("Mensaje de la rama de correcion bug#1234");
 	}
 
 }
